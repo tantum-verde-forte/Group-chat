348ed800569a4141d7355c91a1d04c9b46d5e0bb
frees <troshin-aleksey@bk.ru>
2023-09-11 18:47:22 +0300

    Redis domain fix

--- chat/chat/settings.py
+++ chat/chat/settings.py
@@ -62,7 +62,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [('chatredis', 6379)],
+            "hosts": [(os.getenv("DJANGO_REDIS_HOST"), 6379)],
         },
     },
 }
